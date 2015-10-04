887bc229142d2a82192e2d2de92cec3de53ee815
Ezequiel Guitler <eguitler@gmail.com>
2015-10-04 10:31:30 -0300

    model registered in admin, and fix models file

--- firstApp/models.py
+++ firstApp/models.py
@@ -13,8 +13,6 @@ class Question(models.Model):
     def was_published_recently(self):
 	return (self.pub.date >= timezone.now())
 
-datetime.timedelta(days=1)
-
     def __str__(self):              # __unicode__ on Python 2
         return self.question_text
 
@@ -23,10 +21,7 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-<<<<<<< HEAD
     def __str__(self):
 	return (self.choice_text)
-=======
     def __str__(self):              # __unicode__ on Python 2
         return self.choice_text
->>>>>>> 02ee4c004d3d3a2a09b976f2f6af290b1d9ec136
