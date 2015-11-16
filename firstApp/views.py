1ed3381b90d3e7471c2608a668020185d42f86d1
Ezequiel Guitler <eguitler@gmail.com>
2015-11-16 00:52:49 -0300

    Homepage implemented

--- firstApp/views.py
+++ firstApp/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the firsApp index.")
+     return render(request,'firstApp/index.html')
+#    return HttpResponse("Hello, world. You're at the firsApp index.")
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
