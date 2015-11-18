19cfe5af618099ed03007c3b21778ffead287f8f
Ezequiel Guitler <eguitler@gmail.com>
2015-11-17 22:15:54 -0300

    view updated to use forms

--- homeconomics/views.py
+++ homeconomics/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import View
+from forms import myForm
 
-def index(request):
-   return render(request,'homeconomics/index.html')
-   #return HttpResponse("Hello, world. You're at the firsApp index.")
+class Home(View):
 
+    def get(self,request):
+	form = myForm()
+	context = {'content':"CONTENT",
+		   'footer':"FOOTER",
+		   'form':form
+		  }
+	return render(request,'index.html',context)
+
+    def post(self,request):
+	form = myForm(request.POST)
+	if form.is_valid():
+	    text = form.cleaned_data['text']
+	    option = form.cleaned_data['option']
+	context = {'text':text,
+		   'option':option
+		  }
+	return render(request,'response.html',context)
